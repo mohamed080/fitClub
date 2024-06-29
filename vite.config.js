e40d96b574e002518d4ad06ddd3e59f273a85f22
mohamed <mohayman080@gmail.com>
2024-06-29 13:39:10 +0300

    second comment

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
+  base: '/fitClub/', // Ensure the base URL matches your GitHub Pages repo name
 })
